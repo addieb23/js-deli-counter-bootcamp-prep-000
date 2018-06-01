6403d51e3aa03a64e80a96f5d548fddabdd939d9
Addie Brunk <mlbrunk23@gmail.com>
2018-06-01 00:47:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 //var katzDeli = [];
-function takeANumber(katzDeliLine, name){
-  var katzDeli = [];
-  for(let i = 0; i < katzDeliLine.length; i++){
-    katzDeli.push(katzDeliLine[i]);
-  }
-  katzDeli.push(name);
-  return `Welcome, ${name}. You are number ${katzDeli.length} in line.`
+var katzDeli = [];
+ function takeANumber(katzDeliLine, name){
 
+  katzDeliLine.push(name);
+  
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
 
